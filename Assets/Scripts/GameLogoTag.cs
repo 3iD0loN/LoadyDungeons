449c8913b374fb68193a62fafa6ef9268b0c9d80
eric.rico <eric.rico@unity3d.com>
2023-01-11 15:18:09 -0500

    cleaned up GameLogoTag script and added summary

--- Assets/Scripts/GameLogoTag.cs
+++ Assets/Scripts/GameLogoTag.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is strictly used to find the GameLogo GameObject
+/// using this component
+/// </summary>
 public class GameLogoTag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
