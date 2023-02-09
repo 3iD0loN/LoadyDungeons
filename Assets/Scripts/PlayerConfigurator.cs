ae5a3734ca8cb6d275a2a3263d85eec35359070b
Mat Valadez <mat.valadez@unity3d.com>
2023-02-09 10:39:23 -0800

    Copied in hats stuff. Working from local host

--- Assets/Scripts/PlayerConfigurator.cs
+++ Assets/Scripts/PlayerConfigurator.cs
@@ -1,32 +1,69 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 // Used for the Hat selection logic
 public class PlayerConfigurator : MonoBehaviour
 {
-    [SerializeField]
-    private Transform m_HatAnchor;
+	public AssetReference AssetReference;
+	public string Address;
+	[SerializeField]
+	private Transform m_HatAnchor;
+	private AsyncOperationHandle<GameObject> m_handle;
 
-    private ResourceRequest m_HatLoadingRequest;
 
-    void Start()
-    {           
-        SetHat(string.Format("Hat{0:00}", GameManager.s_ActiveHat));
-    }
+	private AsyncOperationHandle m_HatLoadingHandle;
 
-    public void SetHat(string hatKey)
-    {
-        m_HatLoadingRequest = Resources.LoadAsync(hatKey);
-        m_HatLoadingRequest.completed += OnHatLoaded;
-    }
+	void Start()
+	{
+		Debug.LogFormat("Address: {0}", Address);
 
-    private void OnHatLoaded(AsyncOperation asyncOperation)
-    {
-        Instantiate(m_HatLoadingRequest.asset as GameObject, m_HatAnchor, false);
-    }
+		//If the condition is met, then a hat has been unlocked
+		if (GameManager.s_ActiveHat >= 0)
+		{
+			//SetHat(string.Format("Hat{0:00}", UnityEngine.Random.Range(0, 4)));
 
-    private void OnDisable()
-    {
-        if (m_HatLoadingRequest != null)
-            m_HatLoadingRequest.completed -= OnHatLoaded;
-    }
+			// Fetch the correct hat variable from the ApplyRemoteConfigSettings instance
+			//hatKey is an Addressable Label
+			//Debug.Log("Hat String: " + string.Format("Hat{0:00}", UnityEngine.Random.Range(0, 4)));
+		}
+		SetHat(string.Format("Hat{0:00}", UnityEngine.Random.Range(0, 4)));
+		// SetHat(Address);
+	}
+
+	public void SetHat(string hatKey)
+	{
+		Debug.Log(hatKey);
+		m_handle = Addressables.InstantiateAsync(hatKey, m_HatAnchor);
+	}
+
+	// public void SetHat(string hatKey)
+	// {
+	// 	// AsyncOperationHandle<GameObject> asyncOperationHandle = Addressables.LoadAssetAsync<GameObject>(AssetReference);
+	// 	AsyncOperationHandle<GameObject> asyncOperationHandle = Addressables.LoadAssetAsync<GameObject>(hatKey);
+	// 	asyncOperationHandle.Completed += HatOperationHandle_Completed;
+	// }
+
+	// private void HatOperationHandle_Completed(AsyncOperationHandle<GameObject> asyncOperationHandle)
+	// {
+	// 	Debug.Log("AsyncOperationHandle Status: " + asyncOperationHandle.Status);
+	// }
+
+	private void OnDisable()
+	{
+		m_HatLoadingHandle.Completed -= OnHatInstantiated;
+		Addressables.ReleaseInstance(m_handle);
+	}
+
+	private void OnHatInstantiated(AsyncOperationHandle obj)
+	{
+		// We can check for the status of the InstantiationAsync operation: Failed, Succeeded or None
+		if (obj.Status == AsyncOperationStatus.Succeeded)
+		{
+			Debug.Log("Hat instantiated successfully");
+		}
+
+		m_HatLoadingHandle.Completed -= OnHatInstantiated;
+	}
 }
