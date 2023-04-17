081e96637d23628fe497c634a6595589ee5c6dd0
joel-barba <joel.barba@unity3d.com>
2023-04-17 06:03:48 -0500

    Update PlayerConfigurator.cs

--- Assets/Scripts/PlayerConfigurator.cs
+++ Assets/Scripts/PlayerConfigurator.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
 
 // Used for the Hat selection logic
 public class PlayerConfigurator : MonoBehaviour
@@ -9,39 +10,43 @@ public class PlayerConfigurator : MonoBehaviour
     [SerializeField]
     private Transform m_HatAnchor;
 
-    private List<string> m_Keys = new List<string>() { "Hats", "Seasonal" };
+    private List<string> m_Keys = new List<string>() { "Hats", "Fancy" };
 
-    private AsyncOperationHandle<IList<GameObject>> m_HatsLoadOpHandle;
+    private AsyncOperationHandle<IList<IResourceLocation>> m_HatsLocationsOpHandle;
+
+    private AsyncOperationHandle<GameObject> m_HatLoadOpHandle;
 
     private GameObject m_HatInstance;
 
     void Start()
     {
-        m_HatsLoadOpHandle = Addressables.LoadAssetsAsync<GameObject>(m_Keys, null, Addressables.MergeMode.Intersection);
-        m_HatsLoadOpHandle.Completed += OnHatsLoadComplete;
+        m_HatsLocationsOpHandle = Addressables.LoadResourceLocationsAsync(m_Keys, Addressables.MergeMode.Intersection);
+        m_HatsLocationsOpHandle.Completed += OnHatLocationsLoadComplete;
     }
 
-    private void OnHatsLoadComplete(AsyncOperationHandle<IList<GameObject>> asyncOperationHandle)
+    private void OnHatLocationsLoadComplete(AsyncOperationHandle<IList<IResourceLocation>> asyncOperationHandle)
     {
         Debug.Log("AsyncOperationHandle Status: " + asyncOperationHandle.Status);
 
         if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
         {
-            IList<GameObject> results = asyncOperationHandle.Result;
+            IList<IResourceLocation> results = asyncOperationHandle.Result;
             for (int i = 0; i < results.Count; i++)
             {
-                Debug.Log("Hat: " + results[i].name);
+                Debug.Log("Hat: " + results[i].PrimaryKey);
             }
 
             LoadInRandomHat(results);
         }
     }
 
-    private void LoadInRandomHat(IList<GameObject> prefabs)
+    private void LoadInRandomHat(IList<IResourceLocation> resourceLocations)
     {
-        int randomIndex = Random.Range(0, prefabs.Count);
-        GameObject randomHatPrefab = prefabs[randomIndex];
-        m_HatInstance = Instantiate(randomHatPrefab, m_HatAnchor);
+        int randomIndex = Random.Range(0, resourceLocations.Count);
+        IResourceLocation randomHatPrefab = resourceLocations[randomIndex];
+
+        m_HatLoadOpHandle = Addressables.LoadAssetAsync<GameObject>(randomHatPrefab);
+        m_HatLoadOpHandle.Completed += OnHatLoadComplete;
     }
 
     private void Update()
